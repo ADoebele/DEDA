ae9f00d0db0c9ecf80b5e6204581c1bf4511d311
ADoebele <33786532+ADoebele@users.noreply.github.com>
2018-01-18 13:13:01 +0100

    Update Cryptocurriencie_Price_Data.py

--- Cryptocurriencie_Price_Data.py
+++ Cryptocurriencie_Price_Data.py
@@ -1,25 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 13 16:54:54 2017
+import os
 
-@author: Alex
-"""
-#import os
-
-#os.chdir("/Users/AlexD/Desktop/Python Project")
-#path_direct = os.getcwd()
+os.chdir("C:\\Users\\Tomas\\Desktop\\Crypto_Project")
+path_direct = os.getcwd()
 
 '''
 packages plotly and quandl might need to be installed first
 '''
 
 import quandl
+import numpy as np
 import pandas as pd
-#import pickle
 import plotly.offline as py
 import plotly.graph_objs as go
+import datetime 
 
 
+'''
+1. Scrap Bitcoin pricing data
+'''
   
 
 #Define a function to pull the data from one exchange:
@@ -36,8 +34,8 @@ df_kraken = crypto_data('BCHARTS/KRAKENUSD')
 df_kraken.head() #Shows the first five columns of our dataframe
 
 #First chart of Bitcoin timeseries from Bitstamp:
-btc = go.Scatter(x=df_kraken.index, y=df_kraken['Weighted Price'])
-py.plot([btc],filename='btc_USD_Kraken.html')
+kraken_single = go.Scatter(x=df_kraken.index, y=df_kraken['Weighted Price'],name = 'kraken', line = dict(color = '#30BEHF'))
+py.plot([kraken_single],filename='btc_USD_Kraken.html')
 
 
 # Pull pricing data for more BTC exchanges and safe them in a dictionary
@@ -66,4 +64,54 @@ btc_usd_datasets = combine_dataframes(list(exchange_data.values()), list(exchang
 btc_usd_datasets.tail()
 
 #Plotting the results:
-#To be continued...
+bitstamp = go.Scatter(
+           x = btc_usd_datasets.index,
+           y = btc_usd_datasets.BITSTAMP,
+           name = 'bitstamp',
+           line = dict(color = '#17BECF'),
+           opacity = 0.8)
+           
+coinbase =go.Scatter(
+          x = btc_usd_datasets.index,
+          y = btc_usd_datasets.COINBASE,
+          name = 'coinbase',
+          line = dict(color =  '#18EFGH'),
+          opacity = 0.8)
+
+itbit  = go.Scatter(
+          x = btc_usd_datasets.index,
+          y = btc_usd_datasets.ITBIT,
+          name = 'itbit',
+          line = dict(color =  '#23HFTP'),
+          opacity = 0.8)
+          
+kraken  = go.Scatter(
+          x = btc_usd_datasets.index,
+          y = btc_usd_datasets.KRAKEN,
+          name = 'kraken',
+          line = dict(color =  '#30BEHF'),
+          opacity = 0.8)
+
+data = [bitstamp,coinbase,kraken,itbit]
+fig = dict(data=data)
+py.plot(fig,filename = 'BTC_USD.html')
+
+#Readjust the plot and drop values of 0
+btc_usd_datasets.replace(0, np.nan, inplace=True)
+
+py.plot(fig, filename = 'BTC_USD.html')
+
+#Average over the weighted prices of all 4 exchanges and plot the result
+
+btc_usd_datasets['avg_BTC_price'] = btc_usd_datasets.mean(axis = 1)
+
+avg_btc_price = go.Scatter(x = btc_usd_datasets.index, y = btc_usd_datasets.avg_BTC_price, name = 'Average BTC_USD price', line = dict(color =  '#14DETF'), opacity = 0.8)
+
+#Plotting the results
+py.plot([avg_btc_price], filename = 'Average_BTC_USD_price')
+
+#Consits only of the average price of bitcoin
+avg_btc_price = btc_usd_datasets[['avg_BTC_price']].copy()
+#Create a time stamp in our usual format
+avg_btc_price.set_index(datetime.datetime(btc_usd_datasets.index.copy(),'%m/%d/%Y, %I:%M %p' ))
+
