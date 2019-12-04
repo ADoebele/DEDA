fdf7289eda3a8e198e5c36dd1d2a548230118761
Alexander Döbele <Alexander.doebele@gmx.de>
2019-12-04 11:52:34 +0100

    Update bitcoinfunc.py

--- bitcoinfunc.py
+++ bitcoinfunc.py
@@ -4,21 +4,22 @@ import datetime
 import os
 import pandas as pd
 import pickle
-# Using the requests module to get source code from the url
+
 direct = os.getcwd()
 
+#Define 
 def bitcoin_news_scraping(page=1, refresh=False):
-    # Argument page equals 1 by default
+    #Argument page equals 1 by default
     if page == 1:
         # Visit the home page if page equals 1
         nasdaq_url = 'http://www.nasdaq.com/author/bitcoin-magazine'
     else:
-        # Change url by different argument
+        #Change url by different argument
         nasdaq_url = 'http://www.nasdaq.com/author/bitcoin-magazine?page=' + str(page)
     if (not os.path.exists(direct + '\\temp\\' + str(page) + '.pkl')) or (refresh is True):
-        # connect to the website if the webpage source code file is not exist of we need to refresh it
+        #Connection to the website
         url_request = requests.get(nasdaq_url)
-        # save the request object after scraping
+        #Save the request object after scraping
         with open(direct + '\\temp\\' + str(page) + '.pkl', 'wb') as url_file:
             pickle.dump(url_request, url_file)
     else:
@@ -28,12 +29,12 @@ def bitcoin_news_scraping(page=1, refresh=False):
 
     url_request = requests.get(nasdaq_url)
     url_content = url_request.content
-    # Using BeautifulSoup to parse webpage source code
+    #Use BeautifulSoup to parse webpage source code
     parsed_content = soup(url_content)
-    # Finding all the <li> tag content
+    #Finde all the <li> tag content
     containers = parsed_content.find_all('li')
 
-    # initial empty list to load the data
+    #Initial empty list to load the data
     link_list = []
     title_list = []
     time_list = []
@@ -41,7 +42,7 @@ def bitcoin_news_scraping(page=1, refresh=False):
 
     for i in range(0, 20):
         for container in containers:
-            # Using .find_all() method to search tag name and attribute
+            #Use .find_all() method to search tag name and attribute
             ident_a = "main_content_rptArticles_hlArticleLink_{}".format(i)
             ident_b = "main_content_rptArticles_lbArticleInfo_{}".format(i)
             ident_c = "main_content_rptArticles_lbCategories_{}".format(i)
@@ -50,9 +51,9 @@ def bitcoin_news_scraping(page=1, refresh=False):
             container_c = container.find_all('span', {"id": ident_c})
 
             for item in container_a:
-                # Loop in the <a> tag
+                #Loop in the <a> tag
                 news_link = item.get('href').strip()
-                # strip() method can remove specific chars at the head and tail
+                #strip() method can remove specific chars at the head and tail
                 news_title = item.text.strip()
                 link_list.append(news_link)
                 title_list.append(news_title)
