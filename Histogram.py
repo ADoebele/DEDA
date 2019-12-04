da2f7a4451066bfef1a44ff124ce6a6bcd35de15
Alexander Döbele <Alexander.doebele@gmx.de>
2019-12-04 12:04:24 +0100

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -1,26 +1,23 @@
-'''
-Histogram of words
-'''
-
 import numpy as np
 from collections import Counter
 import matplotlib.pyplot as plt
 
 
-#Only the most frequent 10 words are observed
+#Filter the most common ten words:
 word_list = [item for sublist in magazin_list for item in sublist]
 counts = dict(Counter(word_list).most_common(10))
 labels, values = zip(*counts.items())
 
-#Sort the values by decending order
+#Sort values by decending order:
 indSort = np.argsort(values)[::-1]
 
-#Rearrange your data
+#Rearrange the data:
 labels = dict.keys(counts)
 values = np.array(values)[indSort]
 indexes = np.arange(len(labels))
 bar_width = 0.3
-#Plot the results
+
+#Plot the results:
 plt.bar(indexes, values)
 plt.xticks(indexes + bar_width, labels, size=7)
 plt.title("Most frequent Words in Bitcoinmagazin")
