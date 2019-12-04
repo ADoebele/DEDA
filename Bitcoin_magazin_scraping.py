1394f4f4b1846c7077fc817b3d5f0426454fa918
Alexander Döbele <Alexander.doebele@gmx.de>
2019-12-04 11:49:50 +0100

    Update Bitcoin_magazin_scraping.py

--- Bitcoin_magazin_scraping.py
+++ Bitcoin_magazin_scraping.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 11 18:45:10 2017
-
-@author: Alex
-"""
-#Scrap the data from bitcoin magazin. We are first interested in the link,titel,time and the tag of the first page.
-
-
-'''
-functional programed
-'''
 from bs4 import BeautifulSoup as soup
 import requests
 import datetime
@@ -17,18 +5,15 @@ import os
 import pandas as pd
 import pickle
 
+#Scrap the link,titel,time and the tag of the first Bitcoin Magazin page:
+
 import bitcoinfunc as bc
-
-
-
 
 title_list = []
 time_list = []
 link_list = []
 tag_list = []
 content_list = [] 
-
-
 
 for page_num in range(1,60):
     print("\nThis is Page: ", page_num)
@@ -47,7 +32,7 @@ bitcoin_magazin = zip(title_list, time_list, link_list, tag_list, content_list)
 bitcoin_magazin_df = pd.DataFrame(list(bitcoin_magazin), columns=['title', 'time','link', 'tag', 'content'])
 bitcoin_magazin_df.to_csv('Bitcoin_magazin_news.csv')
 
-# Getting the content of each article
+#Extract content of each article:
 
 for i in range(len(link_list)):
     print("\nThis is Article: ", i)
